3322036a2be76004bb4d8503826142775f971700
Vasbe <vasbeme@gmail.com>
2021-12-09 13:10:47 +0400

    Добавьте файлы проекта.

---
+++ 15_I_1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+namespace _15_I_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите заданное число: ");
+            int c = int.Parse(Console.ReadLine());
+            string input = File.ReadAllText(@"C:\Users\vasbe\Desktop\input.txt");
+            StreamWriter sw = new StreamWriter("C://Users//vasbe/Desktop/testOut.txt", true);
+            string[] lines = input.Split();
+            float[] nums = lines.Select(float.Parse).ToArray();
+            var maxNums =
+            from n in nums
+            where n > c
+            select n/2;
+            foreach(var x in maxNums) 
+            {
+                sw.Write(x.ToString()+ " ");
+            }
+            sw.Close();
+        }
+    }
+}
